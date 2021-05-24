f5e9213b3c7c5fa7b690b7a431e6457bde676d6a
loloman <kirilldd2@gmail.com>
2021-05-25 00:58:05 +0700

    optimized normalizer;

--- analyzer/analyzer_class/normalizer.py
+++ analyzer/analyzer_class/normalizer.py
@@ -1,21 +1,9 @@
 import maru
-import pymorphy2
 
 
 class Normalizer(object):
     def __init__(self):
-        self.morph = pymorphy2.MorphAnalyzer()
         self.analyzer = maru.get_analyzer(tagger='crf', lemmatizer='pymorphy')
-
-    def get_norm_pymorphy(self, word):
-        return self.morph.parse(word)[0].normal_form
-
-    def pymorphy2(self, words):
-        return [self.morph.parse(word)[0].normal_form for word in words]
-
-
-    def maru(self, words):
-        return [word.lemma for word in self.analyzer.analyze(words)]
 
     def normalize_ner(self, words):
         return [word.lemma for word in self.analyzer.analyze(words)]
